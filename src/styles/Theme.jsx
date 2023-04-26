ead9b459d8e51d35e28985ab2351cb7f8eab56d9
Stalin <sta_vla411@hotmail.com>
2023-04-26 00:24:01 -0500

    add theme light

---
+++ src/styles/Theme.jsx
@@ -0,0 +1,15 @@
+export const light = {
+  pink_500: '#ff5d8e',
+  pink_400: '#EB85A3',
+  gray_500: '#A1ABBD',
+  gray_400: '#AAAAAA',
+  gray_300: '#F5F5F5',
+  gray_200: '#F5F5F5',
+  white: '#FFFFFF',
+  black: '#000000',
+
+  font_16: '1rem',
+  font_14: '0.875rem',
+  font_12: '0.75rem',
+  font_10: '0.625rem'
+}
