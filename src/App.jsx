6eff6a1fc53d6b3d753aba8e6caa5917d5f9d6bf
Stalin Acurio <sta_vla411@hotmail.com>
2023-04-27 14:46:17 -0500

    in process

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import GlobalStyles from './styles/GlobalStyles'
 import Menu from './components/Menu'
 import { light } from './styles/Theme'
 import Login from './components/Login'
+import NewProduct from './components/NewProduct'
+import ShoppingCart from './components/ShoppingCart'
 
 function App() {
   return (
@@ -10,7 +12,7 @@ function App() {
       <GlobalStyles theme={light} />
       <ThemeProvider theme={light}>
         <Menu />
-        <Login />
+        <ShoppingCart />
       </ThemeProvider>
     </>
   )
