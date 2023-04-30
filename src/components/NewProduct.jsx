bbd0fb85c35811807d2a77e4cca4a62bd36ee192
Stalin Acurio <sta_vla411@hotmail.com>
2023-04-30 18:02:35 -0500

    refactor and media query

--- src/components/NewProduct.jsx
+++ src/components/NewProduct.jsx
@@ -33,7 +33,6 @@ function NewProduct() {
             <Label htmlFor='text'>Categoría</Label>
             <Input type='text' placeholder='hogar, ropa, maquillaje, etc' />
           </InputsC>
-
           <InputsNumC>
             <InputsC>
               <Label htmlFor='text'>Precio</Label>
@@ -54,7 +53,9 @@ function NewProduct() {
             <Label>Agregar imagenes</Label>
           </InputsC>
         </section>
-        <Button>Subir Producto</Button>
+        <div>
+          <Button>Subir Producto</Button>
+        </div>
       </FormC>
     </MainC>
   )
@@ -65,13 +66,33 @@ export default NewProduct
 const FormC = styled.form`
   width: 100%;
   display: flex;
-  flex-direction: column;
+  flex-wrap: wrap;
   gap: 1.5rem;
-  margin-top: 2rem;
+  margin-top: 1rem;
   section {
+    width: 100%;
     display: flex;
     flex-direction: column;
     gap: 1rem;
+  }
+  div {
+    width: 100%;
+  }
+  @media screen and (min-width: 768px) {
+    margin-top: 0;
+    padding: 2rem 4rem;
+  }
+  @media screen and (min-width: 1280px) {
+    gap: 2rem;
+    div {
+      width: 100%;
+      button {
+        width: auto;
+      }
+    }
+    section {
+      flex: 1;
+    }
   }
 `
 const InputsNumC = styled.div`
@@ -80,6 +101,9 @@ const InputsNumC = styled.div`
   gap: 1rem;
   input {
     width: 100%;
+  }
+  @media screen and (min-width: 1280px) {
+    gap: 2rem;
   }
 `
 const Textarea = styled.textarea`
@@ -90,4 +114,13 @@ const Textarea = styled.textarea`
   color: ${(props) => props.theme.pink_400};
   min-height: 14rem;
   resize: none;
+  font-size: ${(props) => props.theme.font_16};
+  &::placeholder {
+    color: ${(props) => props.theme.gray_400};
+    font-weight: 400;
+  }
+  @media screen and (min-width: 768px) {
+    padding: 0.75rem 1.5rem;
+    font-size: ${(props) => props.theme.font_18};
+  }
 `
