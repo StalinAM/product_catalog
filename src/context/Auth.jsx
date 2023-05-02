ad22d984ad14c8897f30f65c1790bd0fe6721dec
Stalin Acurio <sta_vla411@hotmail.com>
2023-05-02 10:06:58 -0500

    add auth component

---
+++ src/context/Auth.jsx
@@ -0,0 +1,26 @@
+import { onAuthStateChanged } from 'firebase/auth'
+import React, { createContext, useEffect, useState } from 'react'
+import { auth } from '../firebase/firebase'
+
+export const AuthContext = createContext()
+
+function Auth({ children }) {
+  const [currentUser, setCurrentUser] = useState({})
+  console.log(currentUser)
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user)
+    })
+
+    return () => {
+      unsub()
+    }
+  }, [])
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default Auth
