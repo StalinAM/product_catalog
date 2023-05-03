6a76e1eda7b307218336a1d77f553db3b058c99f
Stalin Acurio <sta_vla411@hotmail.com>
2023-05-02 21:40:19 -0500

    add titles and upload images function

--- src/firebase/services.js
+++ src/firebase/services.js
@@ -1,6 +1,8 @@
 import { signInWithEmailAndPassword } from 'firebase/auth'
-import { auth } from './firebase'
-
+import { auth, storage } from './firebase'
+import { v4 } from 'uuid'
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
+// LOGIN
 export const loginUser = async (email, password, navigate) => {
   try {
     await signInWithEmailAndPassword(auth, email, password)
@@ -9,11 +11,18 @@ export const loginUser = async (email, password, navigate) => {
     console.error(error)
   }
 }
-
+// LOGOUT
 export const logoutUser = async () => {
   try {
     await auth.signOut()
   } catch (error) {
     console.log(error)
   }
+}
+// UPLOAD FILE
+export const uploadImages = async (file) => {
+  const storageRef = ref(storage, v4())
+  await uploadBytes(storageRef, file)
+  const url = await getDownloadURL(storageRef)
+  return url
 }
