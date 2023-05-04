078fb13f4bf7709a57576676c3a7575577f451b6
Stalin Acurio <sta_vla411@hotmail.com>
2023-05-04 18:57:43 -0500

    add fetch products context

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import Auth from './context/Auth'
+import FetchProducts from './context/FetchProducts'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Auth>
-      <App />
+      <FetchProducts>
+        <App />
+      </FetchProducts>
     </Auth>
   </BrowserRouter>
 )
