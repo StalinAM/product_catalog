53ab9219fd13875d8b3f57aed5d4bff442bdcd36
Stalin Acurio <sta_vla411@hotmail.com>
2023-05-05 21:33:59 -0500

    create state product

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,18 @@ import NewProduct from './routes/NewProduct'
 import ProductDescription from './routes/ProductDescription'
 import ShoppingCart from './routes/ShoppingCart'
 import Products from './routes/Products'
+import { useState } from 'react'
 
 function App() {
+  const [product, setProduct] = useState({
+    title: '',
+    description: '',
+    category: '',
+    discounted_price: 0,
+    normal_price: 0,
+    product_details: '',
+    images_urls: ''
+  })
   return (
     <>
       <GlobalStyles theme={light} />
@@ -19,9 +29,15 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='login' element={<Login />} />
-          <Route path='new-product' element={<NewProduct />} />
+          <Route
+            path='new-product'
+            element={<NewProduct product={product} setProduct={setProduct} />}
+          />
           <Route path='shopping-cart' element={<ShoppingCart />} />
-          <Route path='catalog' element={<Products />} />
+          <Route
+            path='catalog'
+            element={<Products product={product} setProduct={product} />}
+          />
         </Routes>
       </ThemeProvider>
     </>
