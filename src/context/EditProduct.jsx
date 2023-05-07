412c64cdff14c2adcc0fa3c85bcde2a2e71a6e14
Stalin <sta_vla411@hotmail.com>
2023-05-07 02:53:44 -0500

    add edit product context

---
+++ src/context/EditProduct.jsx
@@ -0,0 +1,28 @@
+import React, { createContext, useEffect, useState } from 'react'
+export const EditProductConext = createContext()
+function EditProduct({ children }) {
+  const [product, setProduct] = useState({
+    title: '',
+    description: '',
+    category: '',
+    discounted_price: 0,
+    normal_price: 0,
+    product_details: '',
+    images_urls: ''
+  })
+  const [submitActive, setSubmitActive] = useState(false)
+  useEffect(() => {
+    if (submitActive) {
+      return
+    }
+  }, [submitActive])
+  return (
+    <EditProductConext.Provider
+      value={{ product, setProduct, submitActive, setSubmitActive }}
+    >
+      {children}
+    </EditProductConext.Provider>
+  )
+}
+
+export default EditProduct
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import Auth from './context/Auth'
+import EditProduct from './context/EditProduct'
 import FetchProducts from './context/FetchProducts'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Auth>
       <FetchProducts>
-        <App />
+        <EditProduct>
+          <App />
+        </EditProduct>
       </FetchProducts>
     </Auth>
   </BrowserRouter>
