df35f81f48f0422a6fc3bb52b5d34947fc374c9e
Stalin <sta_vla411@hotmail.com>
2023-05-07 02:53:08 -0500

    remove product state

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,8 @@ import NewProduct from './routes/NewProduct'
 import ProductDescription from './routes/ProductDescription'
 import ShoppingCart from './routes/ShoppingCart'
 import Products from './routes/Products'
-import { useState } from 'react'
 
 function App() {
-  const [product, setProduct] = useState({
-    title: '',
-    description: '',
-    category: '',
-    discounted_price: 0,
-    normal_price: 0,
-    product_details: '',
-    images_urls: ''
-  })
   return (
     <>
       <GlobalStyles theme={light} />
@@ -29,15 +19,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='login' element={<Login />} />
-          <Route
-            path='new-product'
-            element={<NewProduct product={product} setProduct={setProduct} />}
-          />
+          <Route path='new-product' element={<NewProduct />} />
           <Route path='shopping-cart' element={<ShoppingCart />} />
-          <Route
-            path='catalog'
-            element={<Products product={product} setProduct={product} />}
-          />
+          <Route path='catalog' element={<Products />} />
         </Routes>
       </ThemeProvider>
     </>
