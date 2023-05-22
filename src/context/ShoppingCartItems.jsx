afb65b49ca4af71651ee2b16e615f044c58099ff
Stalin <sta_vla411@hotmail.com>
2023-05-21 21:12:46 -0500

    add quantity of products to cart

--- src/context/ShoppingCartItems.jsx
+++ src/context/ShoppingCartItems.jsx
@@ -1,12 +1,20 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 
 export const ShoppingCartItemsContext = createContext()
 
 function ShoppingCartItems({ children }) {
   const [shoppingCartItems, setShoppingCartItems] = useState([])
+  const [numItem, setNumItem] = useState(0)
+  useEffect(() => {
+    setNumItem(0)
+    shoppingCartItems.map((item) =>
+      setNumItem((prevState) => prevState + item.quantity)
+    )
+  }, [shoppingCartItems])
+
   return (
     <ShoppingCartItemsContext.Provider
-      value={{ shoppingCartItems, setShoppingCartItems }}
+      value={{ shoppingCartItems, setShoppingCartItems, numItem }}
     >
       {children}
     </ShoppingCartItemsContext.Provider>
