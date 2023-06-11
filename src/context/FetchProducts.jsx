147cd5e25ecb8b347659a537dc1bff1cb8a15920
Stalin <sta_vla411@hotmail.com>
2023-06-11 00:04:06 -0500

    add filter for title and category

--- src/context/FetchProducts.jsx
+++ src/context/FetchProducts.jsx
@@ -1,10 +1,12 @@
-import React, { createContext, useEffect, useState } from 'react'
+import React, { createContext, useEffect, useMemo, useState } from 'react'
 import { fetchProducts } from '../firebase/services'
 
 export const FetchProductsContext = createContext()
 
 function FetchProducts({ children }) {
   const [listProducts, setListProducts] = useState([])
+  const [wordFilter, setWordFilter] = useState(null)
+  const [category, setCategory] = useState(null)
 
   const getProducts = async () => {
     const asyncProducts = await fetchProducts()
@@ -13,10 +15,35 @@ function FetchProducts({ children }) {
 
   useEffect(() => {
     getProducts()
+    setCategory(null)
+    setWordFilter(null)
   }, [])
 
+  const filteredList = useMemo(() => {
+    if (wordFilter) {
+      return listProducts.filter((item) =>
+        item.title.toLowerCase().includes(wordFilter.toLowerCase())
+      )
+    }
+    if (category) {
+      return category !== 'todos'
+        ? listProducts.filter((item) =>
+            item.category.toLowerCase().includes(category.toLowerCase())
+          )
+        : listProducts
+    }
+    return listProducts.map((item) => item)
+  })
   return (
-    <FetchProductsContext.Provider value={{ listProducts, getProducts }}>
+    <FetchProductsContext.Provider
+      value={{
+        listProductsOrigin: listProducts,
+        listProducts: filteredList,
+        getProducts,
+        setCategory,
+        setWordFilter
+      }}
+    >
       {children}
     </FetchProductsContext.Provider>
   )
